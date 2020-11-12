7f5c283484b1ae03c87cd22d3d9b4589a32d8f4d
Caio Henrique <caiohr2020@gmail.com>
2020-11-12 14:17:34 -0300

    Refatoração do gerador de dados | Implementação de rotas

--- BACKEND/README.md
+++ BACKEND/README.md
@@ -1,4 +1,13 @@
-# Start JSON-SERVER
-- Run: npm i -g json-server
-- Enter folder "employee-server"
-- Run: npx json-server --watch -p 3333 resultsPeople.js
+#Project run
+- https://www.npmjs.com/package/node
+- https://www.npmjs.com/search?q=faker
+- https://www.npmjs.com/package/express
+- https://www.npmjs.com/package/json-server
+- https://www.npmjs.com/package/body-parser
+- https://www.npmjs.com/package/got
+
+----------------------------------------------------------------
+
+#Run
+- npm start
+- npx json-server --watch -p 3333 resultsPeople.js
--- BACKEND/server.js
+++ BACKEND/server.js
@@ -2,18 +2,63 @@ const express = require('express')
 const server = express()
 const bodyParser = require('body-parser')
 const porta = 3000
+const got = require('got')
 
 server.use(bodyParser.json())
 server.use(bodyParser.urlencoded({extended: true}))
 
 
+async function getPerson(id){
+    try {
+        const response = await got('http://localhost:3333/searchResults')  
+        return JSON.parse(response.body)[id]
+    } catch (err) {
+        console.log(err.response.body)
+    }
+}
 
-server.get('/', (req, res) =>{
-    res.send("Olá")
+
+async function insertPerson(person){
+    try {
+        const response = await got.post('http://localhost:3333/searchResults', {
+            json: person,
+            responseType: 'json'
+        })
+        return JSON.parse(response.data)
+    } catch (err) {
+        console.log(err.response)
+    }
+}
+
+
+async function listPeople(){
+    try {
+        const response = await got('http://localhost:3333/searchResults')  
+        return JSON.parse(response.body)
+    } catch (err) {
+        console.log(err.response.body)
+    }
+}
+
+
+server.get('/people', async(req, res) =>{
+
+    const result = await listPeople()
+    res.send(result)
+})
+
+server.get('/people/:id', async(req, res) =>{
+
+    const result = await getPerson(req.params.id)
+    res.send(result)
+})
+
+server.post('/people', async (req, res) =>{
+    const result = await insertPerson(req.body)
+    res.send(result)
 })
 
 
-
 server.listen(porta, ()=>{
-    console.log("iniciado na porta 3000")
+    console.log(`iniciado ${porta}`)
 })
