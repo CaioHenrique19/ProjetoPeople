3079e6f7e344a5b281005c50fe2206350c887698
Caio Henrique <caiohr2020@gmail.com>
2020-11-17 09:59:02 -0300

    Arquivos atualizados

---
+++ BACKEND/employeeResults/resultsPeople.js
@@ -0,0 +1,93 @@
+const faker = require('faker')
+
+
+
+function generalPeople(){
+   return {
+        "loginCoach": faker.internet.userName(),
+        "firstNameCoach": faker.name.firstName(),
+        "nickNameCoach": faker.name.suffix() 
+    }
+}
+
+function generatePeople(){
+    const searchResults = []
+
+
+    for(let id = 0; id < 10; id++){
+
+        searchResults.push({
+            "id": id,
+            "name": faker.name.firstName(),
+            "login": faker.name.prefix(),
+            "nickName": faker.name.suffix(),
+            "email": faker.internet.email(),
+            "contact": faker.phone.phoneNumber(),
+            "admission": faker.random.number(),
+            "admissionReal": faker.random.number() ,
+            "role":{
+                "codeRole": faker.random.number() ,
+                "nameRole": faker.name.jobTitle() ,
+                },
+            "area":{
+                "codeArea": faker.random.number() ,
+                "nameArea": faker.name.jobArea() ,
+                "beginArea": null
+            },
+            "project": {
+                "codeProject": faker.random.number() ,
+                "nameProject": faker.lorem.word() 
+            },
+            "cityBase":{
+                "codeCityBase": faker.random.number() ,
+                "nameCityBase": faker.address.state() ,
+                "acronymCityBase": faker.address.state() ,
+                "locationCityBase": `${id}º Andar proximo a entrada`    
+            },
+            "company":{
+                "codeCompany": faker.random.number() ,
+                "nameCompany": "CIT Software S.A."
+            },
+            "timeLine":[
+                {
+                    "areaTimeLine": faker.name.jobArea(),
+                    "projectTimeLinet": faker.random.word(),
+                    "teamTimeLine": faker.random.word(),
+                    "beginDateTimeLine": faker.date.past()
+                }
+            ],
+            "team":[
+                {
+                    "loginTeam": faker.internet.userName(),
+                    "firstNameTeam": faker.name.prefix(),
+                    "nickNameTeam": faker.name.suffix(),
+                    "pictureTeam": faker.image.people()
+                }
+            ],
+            "coach":generalPeople(),
+            "pdm":generalPeople(),
+            "bp":generalPeople(),
+            
+            "biograph": faker.lorem.paragraph() ,
+            "avaliable": faker.random.boolean() ,
+            "clientName": faker.company.companyName(),
+            "picture": faker.image.people()  ,
+            "awardsCategories":[
+                {
+                    "nameAwardsCategories": "Team Highlight",
+                    "iconAwardsCategories": faker.image.abstract(),
+                    "awards": [
+                        {
+                            "dateAwardsCategories": faker.date.past() ,
+                            "loginDelivAwardsCategories": faker.name.prefix(),
+                            "commentAwardsCategories": faker.lorem.paragraph() 
+                        }
+                    ]
+                }
+            ]
+        })
+    }
+    return {"searchResults": searchResults}
+}
+
+module.exports = generatePeople
---
+++ server.js
@@ -0,0 +1,135 @@
+const express = require('express')
+const server = express()
+const bodyParser = require('body-parser')
+const nunjucks = require('nunjucks')
+const porta = 3000
+const got = require('got')
+
+
+server.use(express.static("FRONTEND/public"))
+server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({extended: true}))
+
+
+nunjucks.configure('FRONTEND/src', {
+    autoescape: true,
+    express: server,
+    watch: true,
+})
+
+server.set('view engine', '.html')
+
+//GET PERSON
+async function getPerson(name){
+    try {
+        const response = await got(`http://localhost:3333/people`)
+        return JSON.parse(response.body).filter((person) =>{
+            return person.name === name
+        })
+    } catch (err) {
+        console.log(err.response.body)
+    }
+}
+
+
+//GET PEOPLE
+async function listPeople(){
+    try {
+        const response = await got('http://localhost:3333/people')  
+        return JSON.parse(response.body)
+    } catch (err) {
+        console.log(err.response.body)
+    }
+}
+
+//POST
+async function insertPerson(person){
+    try {
+        const response = await got.post('http://localhost:3333/people', {
+            json: person,
+            responseType: 'json'
+        })
+        return JSON.parse(response.data)
+    } catch (err) {
+        console.log(err.response)
+    }
+}
+
+//DELETE
+async function deletePerson(id){
+    try {
+        const response = await got.delete(`http://localhost:3333/people/${id}`, {
+            responseType: 'json'
+        })
+        return JSON.parse(response.data)
+    } catch (err) {
+        console.log(err.response)
+    }
+}
+
+//PUT
+async function changePerson(id,body){
+    try {
+        const response = await got.put(`http://localhost:3333/people/${id}`, {
+            json: body,
+            responseType: 'json'
+        })
+        return JSON.parse(response.data)
+    } catch (err) {
+        console.log(err.response)
+    }
+}
+
+server.get('/', (req, res) =>{
+    return res.render('../src/perfil.html')
+   
+})
+
+server.get('/people', async(req, res) =>{
+
+    const  result = await listPeople()
+    return res.render('../src/perfil.html',{
+        nameEmployee: result[0].name,
+        bp: result[0].bp.firstNameCoach,
+        coach: result[0].coach.firstNameCoach,
+        contact: result[0].contact,
+        pdm: result[0].pdm.firstNameCoach,
+        biograph: result[0].biograph,
+        nickName: result[0].nickName,
+        locateBuilding: result[0].cityBase.locationCityBase,
+        emailEmployee: result[0].email,
+        loginEmployee: result[0].login,
+        companyEmployee: result[0].role.nameRole,
+        teamEmployee: result[0].team[0].firstNameTeam,
+        baseEmployee: result[0].cityBase.nameCityBase,
+        acronymCityBaseEmployee: result[0].cityBase.acronymCityBase,
+        acronymCityBaseEmployee: result[0].cityBase.acronymCityBase
+    })
+})
+
+server.get('/people/:name', async(req, res) =>{
+    
+    const result = await getPerson(req.params.name)
+    res.send(result)
+})
+
+server.post('/people', async (req, res) =>{
+    const result = await insertPerson(req.body)
+    res.send(result)
+})
+
+server.delete('/people/:id', async (req, res) =>{
+    const result = await deletePerson(req.params.id)
+    res.send(result)
+    
+})
+
+server.put('/people/:id', async (req, res) =>{
+    const result = await changePerson(req.params.id, req.body)
+    res.send(result)
+    
+})
+
+server.listen(porta, ()=>{
+    console.log(`iniciado ${porta}`)
+})
